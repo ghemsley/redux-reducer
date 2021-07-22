2eac2941490ec14c5523dc8d8e7f1bcd5f447aab
ghemsley <ghemsley@protonmail.ch>
2021-07-22 15:48:38 -0500

    commit tree db34f12bcc3923c59858df7745e788bfa4cc0614

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action) {
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return { friends: [...state.friends, action.friend] }
+    case 'REMOVE_FRIEND':
+      const newFriends = [...state.friends]
+      const index = newFriends.findIndex((element) => element.id === action.id)
+      if (index >= 0) {
+        newFriends.splice(index, 1)
+      }
+      return { friends: newFriends }
+    default:
+      return Object.assign({}, state)
+  }
 }
